5deae7b6be0809e50bab4cd86a3a43fcb82f2f67
Win94js <winthuaung6996@gmail.com>
2026-08-06 11:20:03 +0630

    First Step of Learnig NextJS

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import { ChildProcess } from "child_process";
+import type  {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: "Next.js",
+    description: "Next Js Application Building."
+}
+
+export default function RootLayout({
+    children,
+}:{
+    children: React.ReactNode;
+}){
+    return (
+        
+        <html lang="en">
+        {/* ဒီ <head> နေရာမှာပါသမျှတွေက Metadata ကတာဝန်ယူပေးတယ် */}
+        <body>
+            {children}
+        </body>
+        </html>
+    )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home(){
+    return (
+    <main>
+      <h1>Welcome to My Website</h1>
+      <p>This is the home page content.</p>
+    </main>
+  );
+}
