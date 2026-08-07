79171a1fbe733f1a49d223b120d636ec1f548e30
Win94js <winthuaung6996@gmail.com>
2026-08-07 11:40:11 +0630

    adding page layout style header and footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import { ChildProcess } from "child_process";
 import type  {Metadata} from "next";
 
-export const metadata: Metadata = {
-    title: "Next.js",
-    description: "Next Js Application Building."
-}
+// export const metadata: Metadata = {
+//     title: "Next.js",
+//     description: "Next Js Application Building."
+// }
 
 export default function RootLayout({
     children,
@@ -16,7 +16,23 @@ export default function RootLayout({
         <html lang="en">
         {/* ဒီ <head> နေရာမှာပါသမျှတွေက Metadata ကတာဝန်ယူပေးတယ် */}
         <body>
+            <header style={{
+                backgroundColor:"lightblue",
+                padding:"1rem"
+            }}>
+                <p>
+                    Header
+                </p>
+            </header>
             {children}
+            <footer
+            style={{
+                backgroundColor:"ghostwhite",
+                padding:"1rem"
+            }}>
+                <p>Footer</p>
+            </footer>
+
         </body>
         </html>
     )
