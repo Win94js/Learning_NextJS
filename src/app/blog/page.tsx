4328eb99754eadb95a95bbd7191c24e4b2fed736
Win94js <winthuaung6996@gmail.com>
2026-08-08 15:57:57 +0630

    loading UI

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -9,6 +9,11 @@ export const metadata: Metadata = {
     },
 };
 
-export default function Blog(){
+export default async function Blog(){
+    await new Promise(resolve => {
+        setTimeout(() => {
+            resolve("Intentional Delay")
+        }, 2000)
+    })
     return <h1>My Blog Page</h1>
 }
