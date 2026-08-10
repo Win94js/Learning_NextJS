4f8ed4cd4d2f6bb97e541ccb2cbc621e87019db0
Win94js <winthuaung6996@gmail.com>
2026-08-10 15:21:01 +0630

    recover from error

---
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import { useRouter } from "next/navigation";
+import { startTransition } from "react";
+
+
+
+export default function ErrorBoundary({error,reset}:{error:Error, reset: () => void;}){
+     
+    const router = useRouter();
+    const reload = () => {
+        startTransition(() => {
+            router.refresh();
+            reset();
+        })
+    }
+    return (<div>
+        {error.message}
+        {/* <button onClick={() => reset()}>Try Again</button> */}
+        <button onClick={reload}>Try Again</button>
+
+        </div>)
+}
